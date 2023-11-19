33707c2957dd937d8ee53d141fd04dfb53902ecf
Rushen <66794838+15rufernan@users.noreply.github.com>
2023-11-19 13:14:06 +0000

    Initial commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    //config params
+    int score = 0;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
